bb5f77e55c36a9635cf0c666c94d4083b01e13f4
Ashwith45 <ashwith0411@gmail.com>
2025-03-04 11:09:41 +0530

    commit

--- src/homeindex.js
+++ src/homeindex.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import Todo from './todolist';
-import Count from './count';
 import App1 from './home';
+
 
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( <
     React.StrictMode >
-    <
-    App / >
-    <
-    Todo / >
-    <
-    Count / >
+
     <
     App1 / >
 
