7a6b29f284fb699a2141dba7042cef17bd2d1a48
Ashwith45 <ashwith0411@gmail.com>
2025-03-08 14:22:33 +0530

    Theme Context Commit

---
+++ src/ThemeContext/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./About.css";
+
+const About = () => {
+    return ( <
+        div className = "about-container" >
+        <
+        h2 > About Page < /h2> <
+        p > This is the About Page. < /p> <
+        /div>
+    );
+};
+
+export default About;
---
+++ src/ThemeContext/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Contact.css";
+
+const Contact = () => {
+    return ( <
+        div className = "contact-container" >
+        <
+        h2 > Contact Page < /h2> <
+        p > This is the Contact Page. < /p> <
+        /div>
+    );
+};
+
+export default Contact;
